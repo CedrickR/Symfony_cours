fd9365ef92c098fca14f49adc4f3d1431eb22572
cedrick <ckrom26@gmail.com>
2017-09-27 11:32:52 +0200

    Avant Class Form

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,6 +7,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\User;
 
+// Pour la création des formulaires
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 class DefaultController extends Controller
 {
     /**
@@ -15,10 +20,11 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ));
+        // // replace this example code with whatever you need
+        // return $this->render('default/liste.html.twig', array(
+        //     'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        // ));
+        return $this->redirectToRoute('liste');
     }
 
     /**
@@ -28,17 +34,54 @@ class DefaultController extends Controller
     public function nouveauAction(Request $request, $nom, $prenom, $age)
     {
          // replace this example code with whatever you need
-         $em = $this->getDoctrine()->getManager();
-         $user = new User();
-         $user->setNom($nom);
-         $user->setPrenom($prenom);
-         $user->setAge($age);
-         $em->persist($user);
-         $em->flush();
-         return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ));
+        //  $em = $this->getDoctrine()->getManager();
+        //  $user = new User();
+        //  $user->setNom($nom);
+        //  $user->setPrenom($prenom);
+        //  $user->setAge($age);
+        //  $em->persist($user);
+        //  $em->flush();
+        //  return $this->render('default/index.html.twig', array(
+        //     'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        // ));
+
+    }
+    /**
+     * @Route("/ajouter", name="ajouter")
+     *
+     */
+    public function ajouterAction(Request $request)
+    {
+
             
+        $user = new User();
+        $form = $this->createFormBuilder($user)
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
+            ->add('age', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Créer un utilisateur'))
+            ->getForm();
+
+
+        $form->handleRequest($request);
+        
+            if ($form->isSubmitted() && $form->isValid()) {
+                // $form->getData() holds the submitted values
+                // but, the original `$task` variable has also been updated
+                $user = $form->getData();
+        
+                // ... perform some action, such as saving the task to the database
+                // for example, if Task is a Doctrine entity, save it!
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
+        
+                return $this->redirectToRoute('homepage');
+            }
+            
+
+        return $this->render('default/ajouter.html.twig', array('form' => $form->createView()
+        ));
     }
 
 
@@ -53,7 +96,7 @@ class DefaultController extends Controller
             ->getRepository('AppBundle:User');
 
          $utilisateurs = $repository->findAll();
-         //$utilisateurs = $repository->findByAge('Cedrick');
+
         
          return $this->render('default/liste.html.twig', array(
              'utilisateurs' => $utilisateurs ,
@@ -61,10 +104,10 @@ class DefaultController extends Controller
      }
 
     /**
-     * @Route("/utilisateur/{id}", name="utilisateur")
+     * @Route("/detail/{id}", name="detail")
      *
      */
-     public function utilisateurAction(Request $request, $id)
+     public function detailAction(Request $request, $id)
      {
         $repository = $this->getDoctrine()
         ->getRepository('AppBundle:User');
@@ -83,15 +126,55 @@ class DefaultController extends Controller
      */
      public function supprimerAction(Request $request, $id)
      {
+        $em = $this->getDoctrine()->getManager();
         $repository = $this->getDoctrine()
         ->getRepository('AppBundle:User');
 
             $utilisateur = $repository->find($id);
-
+        $em->remove($utilisateur);
+        $em->flush();
             
-            return $this->render('default/detail.html.twig', array(
-                'utilisateur' => $utilisateur ,
+            return $this->render('default/info.html.twig', array(
+                'message' => "L'utilisateur a été supprimé de la table." ,
      ));
+     }
+    
+     /**
+     * @Route("/modifier/{id}", name="modifier")
+     *
+     */
+     public function modifierAction(Request $request, $id)
+     {
+        
+        $repository = $this->getDoctrine()->getRepository("AppBundle:User");
+        $user = $repository->find($id);
+        
+        $form = $this->createFormBuilder($user)
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
+            ->add('age', TextType::class)
+            ->add('save', SubmitType::class, array('label' => "Modifier l'utilisateur"))
+            ->getForm();
+
+
+      $form->handleRequest($request);
+        
+            if ($form->isSubmitted() && $form->isValid()) {
+                $user = $form->getData();
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
+        
+                return $this->redirectToRoute('homepage');
+            }
+   
+    
+        return $this->render(
+            'default/ajouter.html.twig', 
+            array(
+                'message' => "L'utilisateur a été modifié de la table." ,
+                'form' => $form->createView()
+            ));
      }
 
 
